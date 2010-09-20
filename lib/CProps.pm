c49662debfe488bf4841017210a4c577beea6764
Tom Feist <shabble@cowu.be>
2010-09-20 18:22:14 +0100

    fixed it to use LIB directive rather than MYEXTLIB, which seemed like a bit of a hack

--- lib/CProps.pm
+++ lib/CProps.pm
@@ -2,13 +2,11 @@ package CProps;
 
 use Cwd;
 use Inline C
-  => Config
-#  => ENABLE => 'AUTOWRAP'
-#  => PREFIX => 'cp_'
-  => TYPEMAPS => getcwd() . "/typemap"
-  => MYEXTLIB => '/opt/local/lib/libcprops.dylib'
-  => INC => '/opt/local/include/cprops'
-  => FORCE_BUILD => 1;
+ => Config
+ => TYPEMAPS    => getcwd() . "/typemap"
+ => LIBS        => '-L/opt/local/lib -lcprops'
+ => INC         => '/opt/local/include/cprops'
+ => FORCE_BUILD => 1;
 
 use Inline 'C' => 'src/cprops.c';
 
