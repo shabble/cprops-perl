100f737d9ef2babf83700cba29b14152fbff907e
Tom Feist <shabble@cowu.be>
2010-09-22 15:30:31 +0100

    added a bunch of debugging printfs, also using extended trie constructor with copy/delete function ptrs to see what happens. Nothing conclusive yet.

--- CProps-Trie/src/cprops.c
+++ CProps-Trie/src/cprops.c
@@ -5,10 +5,33 @@
 #include <cprops/trie.h>
 #include <cprops/vector.h>
 
+
+void _print_type(SV*);
 // Docs: http://cprops.sourceforge.net/cp_trie.3.html
 
+
+void _trie__node_destructor(void *ptr);
+void* _trie__node_copier(void *ptr);
+
+
+void _trie__node_destructor(void *ptr) {
+    printf("Destructor called on node %p\n", ptr);
+}
+
+void* _trie__node_copier(void *ptr) {
+    printf("Copy called on node %p\n", ptr);
+    return ptr;
+}
+
+
+
 cp_trie *_trie_create() {
-    cp_trie *new = cp_trie_create(COLLECTION_MODE_NOSYNC);
+    cp_trie *new = cp_trie_create_trie
+        (
+            COLLECTION_MODE_NOSYNC | COLLECTION_MODE_COPY,
+            &_trie__node_copier,
+            &_trie__node_destructor
+        );
     return new;
 }
 
@@ -21,9 +44,10 @@ int _trie_count(cp_trie *trie) {
 }
 
 SV* _trie_add(cp_trie *trie, char *key, SV *val) {
-    void *value;
 
+    void *value;
     value = (void *) newRV_inc(val);
+    printf("Adding key %s with valptr: %p\n", key, value);
 
     int ret = cp_trie_add(trie, savepv(key), value);
 
@@ -37,11 +61,65 @@ SV* _trie_add(cp_trie *trie, char *key, SV *val) {
 SV* _trie_remove(cp_trie *trie, char *key) {
     void *node;
     int ret = cp_trie_remove(trie, key, &node);
-    if (ret == 1 && node != NULL) {
-        SV *sv = (SV *)node;
-        return SvRV(sv);
+    /* success?! according to docs, 0 is success but code appears to show that
+     * 1 is success */
+    if (ret == 1) {
+        if (node != NULL) {
+            SV *sv = (SV *)node;
+            //printf("Returning ptr: %p, refok: %d\n", sv, SvROK(sv));
+            printf("SV in remove is: ");
+            _print_type(sv);
+            printf(" ptr: %p\n", sv);
+            return SvRV(sv);
+        } else {
+            printf("Remove succeeded but node NULL\n");
+            return newSV(0);
+        }
     } else {
+        printf("Remove %s failed?\n", key);
         return newSV(0);
+    }
+}
+
+void _print_type(SV *sv) {
+
+    if (SvROK(sv)) {
+        int type = SvTYPE(SvRV(sv));
+
+        switch (type) {
+        case SVt_IV:
+            printf("Scalar (Int)");
+            break;
+        case SVt_NV: 
+            printf("Scalar (NV)");
+            break;
+        case SVt_PV:
+            printf("Scalar (Str)");
+            break;
+        case SVt_RV:
+            printf("Scalar (Ref)");
+            break;
+        case SVt_PVAV:
+            printf("Array");
+            break;
+        case SVt_PVHV:
+            printf("Hash");
+            break;
+        case SVt_PVCV:
+            printf("Code");
+            break;
+        case SVt_PVGV:
+            printf("Glob or FH");
+            break;
+        case SVt_PVMG:
+            printf("Blessed or Magical Scalar");
+            break;
+        default:
+            printf("Default: %d", type);
+        }
+    
+    } else {
+        printf("Not a Reference");
     }
 }
 
@@ -64,10 +142,13 @@ void _trie_prefix_match(cp_trie *trie, char *prefix) {
 }
 
 SV* _trie_exact_match(cp_trie *trie, char *key) {
-    void *match;
-    match = cp_trie_exact_match(trie, key);
-    if (match != NULL) {
-        SV* sv = (SV *)match;
+    void *node;
+    node = cp_trie_exact_match(trie, key);
+    if (node != NULL) {
+        SV* sv = (SV *) node;
+        printf("get returning ptr %p: ", sv);
+        _print_type(sv);
+        printf("\n");
         return SvRV(sv);
     } else {
         return newSV(0);
